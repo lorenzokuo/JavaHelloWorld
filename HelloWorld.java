87ffb39781eb71c8baa50fab269e0a9d718a994a
Lorenzokuo <lorenzokuo@gmail.com>
2018-10-01 23:55:37 -0700

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// public class name must matches with file name
+public class HelloWorld {
+	// we need entry point, Methods, name "main"
+	// entry point is for interpreter to execute code
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
